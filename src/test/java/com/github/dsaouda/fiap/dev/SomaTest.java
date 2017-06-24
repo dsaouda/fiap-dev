48830cdf6f136c1602473ddf2de68a516040b08e
Diego Saouda <dsaouda@gmail.com>
2017-06-24 10:04:20 -0300

    adicionado coverage

--- src/test/java/com/github/dsaouda/fiap/dev/SomaTest.java
+++ src/test/java/com/github/dsaouda/fiap/dev/SomaTest.java
@@ -10,9 +10,10 @@ public class SomaTest {
 	public void somaDeveSer10Test() {
 		
 		float resultado = 10.0f;
-		Soma soma2 = new Soma(5, 5);
+		Soma soma = new Soma(5, 5);
 		
-		assertEquals("5 + 5 = 10", resultado, soma2.getResultado(), 0.0f);
+		assertTrue(soma.getClass() == Soma.class);
+		assertEquals("5 + 5 = 10", resultado, soma.getResultado(), 0.0f);
 	}
 	
 }
